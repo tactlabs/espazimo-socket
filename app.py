95172aad59b7b36dbd16af0ee1c59f805a0a905b
VedhaSankar <vedhasankar26@gmail.com>
2021-05-13 22:04:52 +0530

    Base commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask , render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+@app.route("/admin", methods = ['GET', 'POST'])
+def admin():
+    return render_template("index.html")
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    return render_template("message.html")
+
+@socketio.on('connect')
+def connected():
+    print('connect')
+
+@socketio.on('message')
+def message(json, methods=['GET']):
+    #print(json)
+    socketio.emit('message_response', json)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
