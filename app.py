cbe2fdb5d5335a6ae47983e17b5c2be32383127b
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-15 20:53:02 +0530

    json storing questions done, FINALYY

--- app.py
+++ app.py
@@ -26,16 +26,43 @@ def connected():
 @socketio.on('push-question')
 def message(jestor_push, methods = ['GET']):
     #print(json)
-    update_json(jestor_push)
+    jestor_push = dict_formatter(jestor_push)
+    write_json(jestor_push)
     socketio.emit('message_response', jestor_push)
 
-def update_json(data):
-    json_file = open("questions.json")
-    json_data = json.load(json_file)
-    json_data.update(data)
-    with open('questions.json','w') as questions :
-        json.dump(json_data,questions)
-    return 0
+# def update_json(data):
+#     json_file = open("questions.json")
+#     json_data = json.load(json_file)
+#     json_data.update(data)
+#     with open('questions.json','w') as questions :
+#         json.dump(json_data,questions)
+#     return 0
+
+def dict_formatter(data):
+    new_dict = {
+	data["q_number"] : {
+		"question" : data["question"],
+		"options1" : data["option_1"],
+        "options2" : data["option_2"],
+        "options3" : data["option_3"],
+        "options4" : data["option_4"],
+        "answer"   : data["answer"]
+    }
+		
+	}
+
+    return new_dict
+
+
+def write_json(new_data, filename='questions.json'):
+    with open(filename,'r+') as file:
+        file_data = json.load(file)
+        file_data["questions"].append(new_data)
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
+ 
+    # python object to be appended.
+
 
 @socketio.on('submit-answer')
 def message(json, methods = ['GET']):
