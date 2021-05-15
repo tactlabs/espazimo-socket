4fb262386c4e43939a480e653fb40033c6936d2c
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-15 19:46:08 +0530

    json append issue

--- app.py
+++ app.py
@@ -24,11 +24,18 @@ def connected():
     print('connect')
 
 @socketio.on('push-question')
-def message(json1, methods = ['GET']):
+def message(jestor_push, methods = ['GET']):
     #print(json)
+    update_json(jestor_push)
+    socketio.emit('message_response', jestor_push)
+
+def update_json(data):
+    json_file = open("questions.json")
+    json_data = json.load(json_file)
+    json_data.update(data)
     with open('questions.json','w') as questions :
-        json.dump(json1,questions)
-    socketio.emit('message_response', json1)
+        json.dump(json_data,questions)
+    return 0
 
 @socketio.on('submit-answer')
 def message(json, methods = ['GET']):
@@ -49,12 +56,12 @@ def message(json, methods = ['GET']):
 
     socketio.emit('reveal-answers', result_json)
 
-def random_name():
-    names = ['stupendous_saturn', 'jolly_jupiter', 'marvelous_mars']
+# def random_name():
+#     names = ['stupendous_saturn', 'jolly_jupiter', 'marvelous_mars']
 
-    n = random.randint(0,2)
+#     n = random.randint(0,2)
 
-    return names[n]
+#     return names[n]
 
 
 
